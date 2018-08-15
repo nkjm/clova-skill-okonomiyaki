4f2caef355944d4095b2cae81d5918fcf9efbc5b
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-08-15 22:09:59 +0900

    Fix

--- intent/order.js
+++ intent/order.js
@@ -1,15 +1,24 @@
 "use strict";
 
 const debug = require("debug")("clova:intent");
+const cache = require("memory-cache");
 
 module.exports = (h) => {
-    const params = [{
-        name: "menu",
-        message: "メニューはどうしましょう？"
-    },{
-        name: "quantity",
-        message: "何個ですか？"
-    }];
+    const memory = cache.get(h.getSessionId());
+
+    let params;
+
+    if (memory){
+        params = memory;
+    } else {
+        params = [{
+            name: "menu",
+            message: "メニューはどうしましょう？"
+        },{
+            name: "quantity",
+            message: "何個ですか？"
+        }];
+    }
 
     const slots = h.getSlots();
 
@@ -24,15 +33,28 @@ module.exports = (h) => {
         }
     }
 
+    let complete = true;
+
     for (let p of params){
         if (!p.value){
-           debug(`Going to collect ${p.name}.`);
-           h.setSimpleSpeech({
-               lang: "ja",
-               type: "PlainText",
-               value: p.message,
-           });
-           break;
+            complete = false;
+            debug(`Going to collect ${p.name}.`);
+            h.setSimpleSpeech({
+                lang: "ja",
+                type: "PlainText",
+                value: p.message
+            });
+            break;
         }
     }
+
+    if (complete){
+        h.setSimpleSpeech({
+            lang: "ja",
+            type: "PlainText",
+            value: "了解しました。"
+        });
+    }
+
+    cache.put(h.getSessionId(), params);
 }
