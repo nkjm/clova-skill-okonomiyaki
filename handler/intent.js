515d06fb059f602fed6410ae187970e381b8b628
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-08-15 21:23:44 +0900

    Fix

--- handler/intent.js
+++ handler/intent.js
@@ -5,4 +5,10 @@ const debug = require("debug")("clova:*");
 module.exports = (h) => {
     debug(h.getIntentName());
     debug(h.getSessionId());
+
+    h.setSimpleSpeech({
+        lang: 'ja',
+        type: 'PlainText',
+        value: 'いらっしゃいませ。',
+    });
 }
