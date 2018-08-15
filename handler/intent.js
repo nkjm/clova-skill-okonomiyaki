addf04c7b00503a08a4d499b7374248edb6e1bb1
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-08-15 22:52:24 +0900

    Fix

--- handler/intent.js
+++ handler/intent.js
@@ -3,10 +3,6 @@
 const debug = require("debug")("clova:handler");
 
 module.exports = (h) => {
-    debug(h.getIntentName());
-    debug(h.getSessionId());
-    debug(h.getSlots());
-
     const intent_name = h.getIntentName();
     const intent = require(`../intent/order.js`);
     return intent(h);
