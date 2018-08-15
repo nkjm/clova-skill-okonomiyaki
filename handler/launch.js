7df95774453447a7ff3c39546df146875ab78bed
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-08-15 21:11:39 +0900

    First commit

---
+++ handler/launch.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = (h) => {
+    h.setSimpleSpeech({
+        lang: 'ja',
+        type: 'PlainText',
+        value: 'いらっしゃいませ。',
+    });
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+require("dotenv").config();
+
+const express = require("express");
+const server = express();
+const debug = require("debug")("clova:*");
+const clova = require("@line/clova-cek-sdk-nodejs");
+const launch_handler = require("./handler/launch");
+const intent_handler = require("./handler/intent");
+const session_end_handler = require("./handler/session_end");
+
+server.listen(process.env.PORT || 5000, () => {
+    console.log("Server is running...");
+});
+
+const handler = clova.Client
+.configureSkill()
+.onLaunchRequest(launch_handler)
+.onIntentRequest(intent_handler)
+.onSessionEndedRequest(session_end_handler)
+.handle();
+
+const clovaMiddleware = clova.Middleware({ applicationId: process.env.CLOVA_APP_ID });
+
+/**
+Router configuration
+*/
+server.post('/clova', clovaMiddleware, handler);
