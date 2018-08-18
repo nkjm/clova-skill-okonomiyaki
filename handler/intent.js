0cfb901b2b33a6f064f5e6dbe72b043ea0b03950
Kazuki Nakajima <kazuki.nakajima@linecorp.com>
2018-08-18 11:55:01 +0900

    Add payment

--- handler/intent.js
+++ handler/intent.js
@@ -1,9 +1,124 @@
 "use strict";
 
 const debug = require("debug")("clova:handler");
+const cache = require("memory-cache");
+const flex = require("../lib/flex");
 
-module.exports = (h) => {
-    const intent_name = h.getIntentName();
-    const intent = require(`../intent/order.js`);
-    return intent(h);
+// Setup SDK for Messaging API
+const bot_sdk = require("@line/bot-sdk");
+const bot = new bot_sdk.Client({
+    channelAccessToken: process.env.BOT_ACCESS_TOKEN,
+    channelSecret: process.env.BOT_CHANNEL_SECRET
+});
+
+// Setup SDK for Pay
+const pay_sdk = require("line-pay");
+const pay = new pay_sdk({
+    channelId: process.env.PAY_CHANNEL_ID,
+    channelSecret: process.env.PAY_CHANNEL_SECRET,
+    isSandbox: true
+});
+
+module.exports = async (h) => {
+    /**
+    Define required parameters.
+    */
+    const params = {
+        menu: {
+            message: "メニューはどうしましょう？"
+        },
+        quantity: {
+            message: "何個ですか？"
+        }
+    };
+
+    /**
+    Initiate context. Context is the memory of this conversation. Storing parameters confirmed or to confirm from user.
+    @typedef {Object} context
+    @prop {Array.<String>} to_confirm - List of parameters we need to confirm to acomplish this intent.
+    @prop {Object} confirmed - Store confirmed parameter.
+    */
+    let context = cache.get(h.getSessionId());
+    debug(`Currnet context follows.`);
+    debug(context);
+    if (!context){
+        context = {
+            to_confirm: [],
+            confirmed: {}
+        }
+
+        for (let param_key of Object.keys(params)){
+            context.to_confirm.push(param_key);
+        }
+    }
+
+    /**
+    Apply slots to parameters.
+    */
+    let index = 0;
+    if (context.to_confirm.length > 0){
+        const slots = h.getSlots();
+        debug(`Received slots follows.`);
+        debug(slots);
+
+        for (let slot_key of Object.keys(slots)){
+            let applicable_param_key = context.to_confirm.find(param_key => param_key === slot_key);
+            if (applicable_param_key){
+                debug(`Add ${applicable_param_key} to confirmed.`);
+                context.confirmed[applicable_param_key] = slots[slot_key];
+                debug(`Removing ${applicable_param_key} from to_confirm.`);
+                context.to_confirm.splice(index, 1);
+                debug(`Now to_confirm is ${JSON.stringify(context.to_confirm)}.`);
+            }
+        }
+        index++;
+    }
+
+    /**
+    Confirm parameters if we have.
+    */
+    if (context.to_confirm.length > 0){
+        debug(`Going to collect ${context.to_confirm[0]}.`);
+        h.setSimpleSpeech({
+            lang: "ja",
+            type: "PlainText",
+            value: params[context.to_confirm[0]].message
+        });
+
+        debug(`Context follows.`);
+        debug(context);
+        cache.put(h.getSessionId(), context);
+        return;
+    }
+
+    /**
+    Take final action if all parameters are set.
+    */
+    debug(`All params have been filled. Going to take final action..`);
+    // Reply via clova.
+    h.setSimpleSpeech({
+        lang: "ja",
+        type: "PlainText",
+        value: `${context.confirmed.menu}を${context.confirmed.quantity}個ですね。すぐにお届けします。家どこか知らんけど（笑`
+    });
+
+    // Reserve payment using Pay API.
+    let reserve_option = {
+        productName: `お好み焼`,
+        amount: 600, // Should be set dynamically.
+        currency: "JPY",
+        orderId: `${h.getSessionId()}`,
+        confirmUrl: process.env.PAY_CONFIRM_URL,
+        confirmUrlType: "SERVER",
+    }
+    let reservation = await pay.reserve(reserve_option);
+    cache.put(reservation.orderId, reservation);
+
+    // Send message via chatbot.
+    /*
+    let pay_message = flex.pay_message({payment_url: reservation.info.paymentUrl.web});
+    await bot.pushMessage(h.requestObject.session.user.userId, pay_message);
+    */
+
+    h.endSession();
 }
--- handler/launch.js
+++ handler/launch.js
@@ -3,9 +3,6 @@
 const debug = require("debug")("clova:handler");
 
 module.exports = (h) => {
-    debug(h.getIntentName());
-    debug(h.getSessionId());
-
     h.setSimpleSpeech({
         lang: 'ja',
         type: 'PlainText',
